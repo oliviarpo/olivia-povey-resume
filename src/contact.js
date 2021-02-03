0b5b53b3855687b57086a2acd96401de76b76776
oliviarpo <oliviapovey@outlook.com>
2021-02-03 16:22:03 +0000

    Practice using JS
    Added:
    - Contact form with Bootstrap
    - JS change inner html onClick
    - Time and date

---
+++ src/contact.js
@@ -0,0 +1,4 @@
+function validateForm() {
+  document.getElementById("contact").innerHTML =
+    "Thanks! We'll be in touch soon";
+}
--- src/script.js
+++ src/script.js
@@ -1,3 +1,16 @@
+// Current date
+var dt = new Date();
+document.getElementById("datetime").innerHTML =
+  ("0" + dt.getHours()).slice(-2) +
+  ":" +
+  ("0" + dt.getMinutes()).slice(-2) +
+  ", " +
+  ("0" + dt.getDate()).slice(-2) +
+  "." +
+  ("0" + (dt.getMonth() + 1)).slice(-2) +
+  "." +
+  dt.getFullYear();
+
 // When the user scrolls the page, execute myFunction
 window.onscroll = function () {
   myFunction();
